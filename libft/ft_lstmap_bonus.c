f539ce731fcc0903e5c4ad51576687c116532355
Lucas Graça <142546339+gracaflucas@users.noreply.github.com>
2024-05-01 16:48:28 +0100

    Update ft_lstmap_bonus.c

--- libft/ft_lstmap_bonus.c
+++ libft/ft_lstmap_bonus.c
@@ -16,16 +16,20 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*node;
 	t_list	*result;
+	void	*tmp;
 
-	if (f == NULL || del == NULL)
+	if (lst == NULL || f == NULL || del == NULL)
 		return (NULL);
 	result = NULL;
 	while (lst != NULL)
 	{
-		node = ft_lstnew(f(lst->content));
-		if (node == NULL)
+		tmp = (*f)(lst->content);
+		if (tmp)
+			node = ft_lstnew(tmp);
+		if (node == NULL || tmp == NULL)
 		{
-			ft_lstclear(&node, (*del));
+			ft_lstclear(&result, del);
+			del(tmp);
 			return (NULL);
 		}
 		ft_lstadd_back(&result, node);
