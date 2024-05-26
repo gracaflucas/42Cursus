aca00ee6059466a10694282cae41fd8f62b7f99c
Lucas Graça <142546339+gracaflucas@users.noreply.github.com>
2024-05-26 16:58:29 +0100

    Update ft_memmove.c

--- libft/ft_memmove.c
+++ libft/ft_memmove.c
@@ -37,17 +37,3 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 	}
 	return ((unsigned char *)dest);
 }
-
-/*#include <stdio.h>
-#include <string.h>
-
-int main () {
-   char dest[] = "oldstring";
-   const char src[]  = "newstring";
-
-   printf("Before memmove dest = %s, src = %s\n", dest, src);
-   ft_memmove(dest, src, 9);
-   printf("After memmove dest = %s, src = %s\n", dest, src);
-
-   return(0);
-}*/
