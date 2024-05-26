464a5806bee9f6fa59e5730ac782fb462afb2f6f
Lucas Graça <142546339+gracaflucas@users.noreply.github.com>
2024-05-26 16:59:43 +0100

    Update ft_strchr.c

--- libft/ft_strchr.c
+++ libft/ft_strchr.c
@@ -24,18 +24,3 @@ char	*ft_strchr(const char *s, int c)
 		return ((char *)s);
 	return (NULL);
 }
-
-/*#include <stdio.h>
-#include <string.h>
-
-int main () {
-   const char str[] = "Lucas Graca42";
-   const char ch = 'c';
-   char *ret;
-
-   ret = ft_strchr(str, ch);
-
-   printf("String after |%c| is - |%s|\n", ch, ret);
-   
-   return(0);
-}*/
