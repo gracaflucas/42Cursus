aa21d8dcaa0efd5ad45bea4985dbb89fee4c3fbc
Lucas Graça <142546339+gracaflucas@users.noreply.github.com>
2024-05-26 16:58:14 +0100

    Update ft_memcpy.c

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -26,17 +26,3 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	}
 	return (dest);
 }
-
-/*#include <stdio.h>
-#include <string.h>
-
-int main () {
-   const char src[50] = "Lucas Graca42";
-   char dest[50];
-   strcpy(dest,"Heloooo!!");
-   printf("Before memcpy dest = %s\n", dest);
-   ft_memcpy(dest, src, strlen(src)+1);
-   printf("After memcpy dest = %s\n", dest);
-   
-   return(0);
-}*/
