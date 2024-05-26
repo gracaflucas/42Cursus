6043026d017b56b89e5c7ef36500607e1d0cfca5
Lucas Graça <142546339+gracaflucas@users.noreply.github.com>
2024-05-26 16:58:00 +0100

    Update ft_memcmp.c

--- libft/ft_memcmp.c
+++ libft/ft_memcmp.c
@@ -29,27 +29,3 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 	}
 	return ((int)(*str1 - *str2));
 }
-/*
-#include <stdio.h>
-#include <string.h>
-
-int main () {
-   char str1[15];
-   char str2[15];
-   int ret;
-
-   memcpy(str1, "abcdef", 6);
-   memcpy(str2, "ABCDEF", 6);
-
-   ret = memcmp(str1, str2, 5);
-
-   if(ret > 0) {
-      printf("str2 is less than str1");
-   } else if(ret < 0) {
-      printf("str1 is less than str2");
-   } else {
-      printf("str1 is equal to str2");
-   }
-   
-   return(0);
-}*/
