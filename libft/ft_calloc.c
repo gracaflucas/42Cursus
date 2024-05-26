441ebcde9c74c87f44531650042e112a1ee4fa08
Lucas Graça <142546339+gracaflucas@users.noreply.github.com>
2024-05-26 16:55:32 +0100

    Update ft_calloc.c

--- libft/ft_calloc.c
+++ libft/ft_calloc.c
@@ -11,21 +11,6 @@
 /* ************************************************************************** */
 
 #include "libft.h"
-/*
-void	*ft_memset(void *s, int c, size_t n)
-{
-	size_t	i;
-
-	i = 0;
-	if (s == NULL)
-		return (NULL);
-	while (i < n)
-	{
-		((unsigned char *)s)[i] = (unsigned char)c;
-		i++;
-	}
-	return (s);
-}*/
 
 void	*ft_calloc(size_t nmemb, size_t size)
 {
@@ -37,27 +22,3 @@ void	*ft_calloc(size_t nmemb, size_t size)
 	ft_memset(ptr, 0, nmemb * size);
 	return (ptr);
 }
-/*
-#include <stdio.h>
-
-int main () {
-   int i, n;
-   int *a;
-
-   printf("Number of elements to be entered:");
-   scanf("%d",&n);
-
-   a = (int*)ft_calloc(n, sizeof(int));
-   printf("Enter %d numbers:\n",n);
-   for( i=0 ; i < n ; i++ ) {
-      scanf("%d",&a[i]);
-   }
-
-   printf("The numbers entered are: ");
-   for( i=0 ; i < n ; i++ ) {
-      printf("%d ",a[i]);
-   }
-   free( a );
-   
-   return(0);
-}*/
